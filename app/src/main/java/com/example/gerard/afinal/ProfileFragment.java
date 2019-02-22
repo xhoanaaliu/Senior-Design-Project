1e8a4d267abb03af182569e135e41609b2291406
Ozgun <ozgunozkan95@gmail.com>
2019-02-22 20:50:26 +0300

    Settings Page and its content is added

--- app/src/main/java/com/example/gerard/afinal/ProfileFragment.java
+++ app/src/main/java/com/example/gerard/afinal/ProfileFragment.java
@@ -71,7 +71,6 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         super.onDetach();
     }
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
     @Override
